27ae96331271a02a418178080104075c95896fea
Amber Johnson <amby22@Mary-Johnsons-MacBook-Pro-2.local>
2019-07-30 13:31:07 -0600

    Create input and button on save

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -3,17 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input-button-unit',
   template: `
-    <p>
-      app-input-button-unit works!
-    </p>
+
+    <input #inputElementRef
+          [value]="title"
+          (keyup.enter)="changeTitle($event.target.value)">
+
+     <button (click)="changeTitle(inputElementRef.value)">
+      Save
+    </button>
   `,
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
+    title = 'Hello world';
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
+  }
+
+  changeTitle(newTitle: string) {
+    this.title= newTitle;
   }
 
 }
